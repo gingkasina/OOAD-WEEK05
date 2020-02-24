45b323b37b8c2f9b5aa8fb8d5ccd3864df55c59f
Koson Trachu <koson.trachu@gmail.com>
2020-02-25 01:26:55 +0700

    Update Program.cs

--- Labs/Example/Animal/Animal/Program.cs
+++ Labs/Example/Animal/Animal/Program.cs
@@ -6,19 +6,38 @@ using System.Threading.Tasks;
 
 namespace Animal
 {
-    class Animal
+    class Animal            // 1.1 เพิ่ม class ชื่อ  Animal
     {
-        public void Eat()
+        public void Eat()   // 1.2 เพิ่มเมธอด Eat()
         {
+            Console.WriteLine("eating...");
         }
     }
 
-    class Cat : Animal
+    class Cat : Animal      // 2.1 เพิ่ม class ชื่อ  Animal
     {
-        public void Meow()
+        public void Meow()  // 2.2 เพิ่มเมธอด Eat()
         {
+            Console.WriteLine("meowing...");
         }
     }
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("barking...");    
+        }
+    }
+
+    class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
